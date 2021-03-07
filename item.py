dcc26f52291926ac62284892d4c085651952bb49
AdrianKoczurUEK <s219487@student.uek.krakow.pl>
2021-03-07 14:40:11 +0100

    'backup'

--- item.py
+++ item.py
@@ -2,21 +2,31 @@ from bs4 import BeautifulSoup as BS
 from flask import Flask, render_template, request, url_for
 import requests
 
+
 class Item:
-    id=1
     def __init__(self, item_id):
-        self.item_id=item_id
+        self.item_id = item_id
+        self.item_url = f'https://www.ceneo.pl/{item_id}#tab=reviews'
+        self.item_name = self.getItemName()
+        self.reviewss = Reviews(self.item_id,self.item_name)
+
+    def getItemName(self):
+        page = requests.get(self.item_url)
+        soup = BS(page.content, 'html.parser')
+        return soup.find(class_='js_searchInGoogleTooltip breadcrumbs__item').text
+
+class Reviews:
+    def __init__(self, item_id, item_name):
+        self.item_id = item_id
+        self.item_name = item_name
         self.reviews=[]
         self.getAllRevs(item_id)
-
-    def getItemBlock(self,soup):
+    def getItemBlock(self, soup):
         rev_blocks = soup.find_all(class_="user-post user-post__card js_product-review")
         return rev_blocks
-    def getRevId(self):
-        return self.id
 
-    def getItemRev(self,soup):
-        user_blocks=self.getItemBlock(soup)
+    def getItemRev(self, soup):
+        user_blocks = self.getItemBlock(soup)
         for rev_block in user_blocks:
             # dates
             post_dates = rev_block.find(class_='user-post__published')
@@ -25,48 +35,43 @@ class Item:
             for i in user_dates:
                 number_of_dates += 1
 
-            #recomendations
-            recomendation=''
+            # recomendations
+            recomendation = ''
             if rev_block.find(class_='recommended'):
-                recomendation=rev_block.find(class_='recommended').text
+                recomendation = rev_block.find(class_='recommended').text
             elif rev_block.find(class_='not-recommended'):
-                recomendation=rev_block.find(class_='not-recommended').text
-            #pros and cons
+                recomendation = rev_block.find(class_='not-recommended').text
+            # pros and cons
             pros_list = []
             cons_list = []
             if rev_block.find(class_='review-feature'):
-                col=rev_block.find_all(class_='review-feature__col')
-                if len(col)>1:
-                    pros=col[0].find_all(class_='review-feature__item')
+                col = rev_block.find_all(class_='review-feature__col')
+                if len(col) > 1:
+                    pros = col[0].find_all(class_='review-feature__item')
                     for pro in pros:
                         pros_list.append(pro.text)
                     cons = col[1].find_all(class_='review-feature__item')
                     for con in cons:
                         cons_list.append(con.text)
-                elif len(col)==1:
+                elif len(col) == 1:
                     pros = col[0].find_all(class_='review-feature__item')
                     for pro in pros:
                         pros_list.append(pro.text)
 
-
             # STORE USER INFO IN DICTIONARY
             info = {
-                "id": Item.getRevId(self),
                 "desc": rev_block.find(class_='user-post__text').text,
                 "stars": rev_block.find(class_='user-post__score-count').text,
                 "author": rev_block.find(class_='user-post__author-name').text,
                 "recommended": recomendation,
                 "rev_date": user_dates[0]['datetime'],
                 "buy_date": user_dates[1]['datetime'] if number_of_dates > 1 else '',
-                "pros":pros_list,
-                "cons":cons_list
+                "pros": pros_list,
+                "cons": cons_list
             }
-            self.id+=1
             self.reviews.append(info)
 
-
-
-    def getAllRevs(self,item_id):
+    def getAllRevs(self, item_id):
         url = f'https://www.ceneo.pl/{item_id}#tab=reviews'
         page = requests.get(url)
         soup = BS(page.content, 'html.parser')
